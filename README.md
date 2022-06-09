30b560dd9a0cd312e6d48d2097b33ce8ec3a99e5
sulaiman <sulaimanaminu02@gmail.com>
2022-06-09 22:42:42 +0100

    this is the last commit to publish the api

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # random-user-generator-in-node-js
+
+# this api generate random user
+
+# it expects num as query 
+
+# num is used to specify number of alphabets needed for name length
+
+# feel free to fuck and improve this api
--- src/controllers/randomUserGenerator.js
+++ src/controllers/randomUserGenerator.js
@@ -6,6 +6,10 @@ exports.generateRandomUser = (req, res) => {
   const smallAlphabets = 'abcdefghijklmnopqrstuvwxyz'
   const numbers = '0123456789'
   const genderArr = ['male', 'female']
+  const days = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10',
+    '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
+    '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31']
+  const months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
 
   const { num } = req.query
   const firstName = `${bigAlphabets.charAt(Math.floor(Math.random() * bigAlphabets.length))}${generateRandom(num, smallAlphabets)}`
@@ -15,5 +19,7 @@ exports.generateRandomUser = (req, res) => {
   const username = `${bigAlphabets.charAt(Math.floor(Math.random() * bigAlphabets.length))}${generateRandom(5, smallAlphabets)}${generateRandom(2, numbers)}`
   const password = `${generateRandom(5, smallAlphabets)}${generateRandom(1, specChars)}${generateRandom(2, numbers)}`
   const email = `${generateRandom(7, smallAlphabets)}@${generateRandom(5, smallAlphabets)}.com`
-  res.json({ message: 'everything works well', name: { firstName, lastName }, gender, phone, username, email, password })
+  const dob = `${days[Math.floor(Math.random() * days.length)]}-${months[Math.floor(Math.random() * months.length)]}-${generateRandom(2, numbers)}`
+
+  res.json({ status: 200, bioData: { gender, dob, phone, name: { firstName, lastName } }, username, email, password })
 }
